3c16e7da6446f759dd30161ee6e9af8c81da7201
sayeri15 <149458894+sayeri15@users.noreply.github.com>
2023-11-02 21:33:17 +0530

    Add files via upload

---
+++ Simple Interest on Rs. 200 for 5 years at 5% per year. 200 for 5 years at 5% per year.py
@@ -0,0 +1,6 @@
+p=int(input("Enter the principal amount:"))
+t=int(input("Enter the value of time:"))
+r=int(input("Enter the rate of interest:"))
+simple_interest=(p*t*r)/100
+print(simple_interest)
+
---
+++ leap year.py
@@ -0,0 +1,5 @@
+num=int(input("Enter the year:"))
+if num%4==0:
+    print(num,"is a leap year")
+else:
+    print(num,"is not a leap year") 
